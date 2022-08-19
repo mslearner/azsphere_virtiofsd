837fc5a03aaaae169e4d7957842c436618f50333
mslearner <linux_learner@outlook.com>
2022-08-19 13:19:06 -0700

    added debug

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -378,7 +378,7 @@ impl Sandbox {
             Ok(())
         } else {
             // This is the parent.
-            println!("Process Id of child from root = {}", pid)
+            println!("Process Id of child from root = {}", pid);
            // self.setup_id_mappings(uid, gid, pid)?;
             util::wait_for_child(child); // This never returns.
         }
